e36e9b9719069a1dd991e75e5b11d3ff48af7ebf
survivorbat <djbatcat@gmail.com>
2024-09-12 13:57:58 +0200

    Add lower-level method SchemaToNode

--- scheyaml.go
+++ scheyaml.go
@@ -17,13 +17,12 @@ type config struct{}
 // `description` and `examples` for documentation, `default` for default values and `properties` for listing blocks.
 //
 // There are currently no options available yet
-func SchemaToYAML(schema []byte, _ ...Option) ([]byte, error) {
-	var schemaObject jsonSchema
-	if err := json.Unmarshal(schema, &schemaObject); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal schema to jsonSchema object: %w", err)
+func SchemaToYAML(schema []byte, opts ...Option) ([]byte, error) {
+	rootNode, err := SchemaToNode(schema, opts...)
+	if err != nil {
+		// Not wrapping the error because that was already done
+		return nil, err
 	}
-
-	rootNode := schemaObject.yamlExample()
 
 	result, err := yaml.Marshal(&rootNode)
 	if err != nil {
@@ -31,4 +30,15 @@ func SchemaToYAML(schema []byte, _ ...Option) ([]byte, error) {
 	}
 
 	return result, nil
+}
+
+// SchemaToNode is a lower-level version of SchemaToYAML, but returns the yaml.Node instead of the
+// marshalled YAML.
+func SchemaToNode(schema []byte, _ ...Option) (*yaml.Node, error) {
+	var schemaObject jsonSchema
+	if err := json.Unmarshal(schema, &schemaObject); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal schema to jsonSchema object: %w", err)
+	}
+
+	return schemaObject.yamlExample(), nil
 }
--- scheyaml_test.go
+++ scheyaml_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
 func TestSchemaToYAML_ReturnsExpectedOutput(t *testing.T) {
@@ -27,4 +28,27 @@ func TestSchemaToYAML_ReturnsExpectedOutput(t *testing.T) {
 
 	// If the properties are as expected, test the comments
 	assert.Equal(t, string(expectedData), string(result))
+}
+
+func TestSchemaToNode_ReturnsExpectedOutput(t *testing.T) {
+	t.Parallel()
+	// Arrange
+	inputData, _ := os.ReadFile(path.Join("testdata", "test-schema.json"))
+
+	// Act
+	result, err := SchemaToNode(inputData)
+
+	// Assert
+	require.NoError(t, err)
+
+	expectedData, _ := os.ReadFile(path.Join("testdata", "test-schema-result.yaml"))
+
+	actualData, err := yaml.Marshal(result)
+	require.NoError(t, err)
+
+	// First test the data itself, and quit if it isn't as expected.
+	require.YAMLEq(t, string(expectedData), string(actualData))
+
+	// If the properties are as expected, test the comments
+	assert.Equal(t, string(expectedData), string(actualData))
 }
